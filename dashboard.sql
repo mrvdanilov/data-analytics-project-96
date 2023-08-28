3d8b8d21cf7a8f5f4a6495326848c6e1cb62ce7d
mrvdanilov <81223621+mrvdanilov@users.noreply.github.com>
2023-08-28 14:00:23 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -1,51 +1,35 @@
 -- сюда можно вставить запрос с любой атрибуции с attribution.sql.
 
-with attribution as (
-    with sessions_with_paid_mark as (
-        select
-            *,
-            case
-                -- необходимо выделить все платные метки из данных
-                -- и здесь дополнить / убрать ненужное
-                when medium in ('cpc', 'cpm', 'cpa', 'youtube', 'cpp', 'tg')
-                    then 1
-                else 0
-            end as is_paid
-        from sessions
-    ),
-
-    visitors_with_leads as (
-        select
-            s.visitor_id,
-            s.datetime,
-            s.source as utm_source,
-            s.medium as utm_medium,
-            s.campaign as utm_campaign,
-            l.created_at,
-            l.amount,
-            l.closing_reason,
-            l.status_id,
-            row_number() over (
-                partition by s.visitor_id
-                order by s.is_paid desc, s.datetime desc
-            ) as rn
-        from sessions_with_paid_mark as s
-        left join leads as l
-            on
-                l.visitor_id = s.visitor_id
-                and l.created_at >= s.datetime
-    )
-
-    select *
-    from visitors_with_leads
-    where rn = 1
+with visitors_with_leads as (
+    select
+        s.visitor_id,
+        s.visit_date,
+        s.source as utm_source,
+        s.medium as utm_medium,
+        s.campaign as utm_campaign,
+        l.created_at,
+        l.amount,
+        l.closing_reason,
+        l.status_id,
+        row_number() over (
+            partition by s.visitor_id
+            order by
+                case when s.medium = 'organic' then 0 else 1 end desc,
+                s.visit_date desc
+        ) as rn
+    from sessions as s
+    left join leads as l
+        on
+            s.visitor_id = l.visitor_id
+            and s.visit_date <= l.created_at
 )
 
 select
     utm_source,
     utm_medium,
     percentile_disc(0.90) within group (
-        order by date_part('day', created_at - datetime)
+        order by date_part('day', created_at - visit_date)
     ) as days_to_lead
-from attribution
+from visitors_with_leads
+where rn = 1
 group by 1, 2
